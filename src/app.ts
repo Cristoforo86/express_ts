51c5b86633c919f2beba3fd2d3961cb5400ebe4c
Cristoforo86 <c.carnabuci@gmx.de>
2019-10-02 23:58:03 +0200

    second commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express, { Application, Request, Response, NextFunction  } from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello');
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello");
 });
 
-app.listen(5000, () => {
-    console.log('Server running')
-})
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is  running on ${port}`);
+});
